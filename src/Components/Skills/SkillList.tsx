7129a6491ee7c87d20c99990f34be205ce816cae
Daniel McPherson <daniel@Daniels-MacBook-Pro.local>
2023-09-28 11:54:58 +0100

    Changed the margin-bottom for the items

--- src/Components/Skills/SkillList.tsx
+++ src/Components/Skills/SkillList.tsx
@@ -14,7 +14,7 @@ export default function SkillList(props: Props) {
     return (
         props.skills.map((skill: SkillsType) => {
             return (
-                <div className="w-48 mb-5">
+                <div className="w-48 mb-3.5">
                     <h3 className="text-center text-lg"> {skill.Item} </h3>
                     <Progress value={skill.Progress} colorScheme="green" height="3" />
                 </div>
