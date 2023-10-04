102af47bc687f3d9de54454b1a3b4893e789eb29
Daniel McPherson <48417169+RangerVinven@users.noreply.github.com>
2023-10-04 21:46:29 +0100

    Changed Margin-Bottom to 4

--- src/Components/Skills/SkillList.tsx
+++ src/Components/Skills/SkillList.tsx
@@ -14,7 +14,7 @@ export default function SkillList(props: Props) {
     return (
         props.skills.map((skill: SkillsType) => {
             return (
-                <div className="w-48 mb-3.5">
+                <div className="w-48 mb-4">
                     <h3 className="text-center text-lg"> {skill.Item} </h3>
                     <Progress value={skill.Progress} colorScheme="green" height="3" />
                 </div>
